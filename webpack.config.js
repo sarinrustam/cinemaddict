969634483ec1e1886330af37e39b2178775896cc
Rustam Sarin <sarin.rustam@icloud.com>
2020-09-23 17:15:33 +0300

    добавил docs для github pages

--- webpack.config.js
+++ webpack.config.js
@@ -6,11 +6,11 @@ module.exports = {
   entry: `./src/main.js`,
   output: {
     filename: `bundle.js`,
-    path: path.join(__dirname, `public`)
+    path: path.join(__dirname, `docs`)
   },
   devtool: `source-map`,
   devServer: {
-    contentBase: path.join(__dirname, `public`),
+    contentBase: path.join(__dirname, `docs`),
     watchContentBase: true
   },
   resolve: {
